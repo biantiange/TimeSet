b31049ca298e6c28ca217cb66e23004fb9f2f436
wangtianwt <981003098@qq.cpm>
2020-04-26 17:13:01 +0800

    t

---
+++ TimeSet_andorid/app/src/main/java/com/example/lt/timeset_andorid/Album/Album.java
@@ -0,0 +1,43 @@
+package com.example.lt.timeset_andorid.Album;
+
+/**
+ * 王天
+ */
+public class Album {
+    private int id;
+    private int useId;
+    private String theme;
+    private String albumName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUseId() {
+        return useId;
+    }
+
+    public void setUseId(int useId) {
+        this.useId = useId;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+}
--- TimeSet_andorid/app/src/main/java/com/example/lt/timeset_andorid/Login/Constant.java
+++ TimeSet_andorid/app/src/main/java/com/example/lt/timeset_andorid/Login/Constant.java
@@ -2,5 +2,6 @@ package com.example.lt.timeset_andorid.Login;
 
 public class Constant {
     public static final String IP ="";
+    public static final   String URL="http://192.168.10.2:8080/";
 
 }
