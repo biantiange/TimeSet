27d175456e0683676818e41b8f96eb6ec5b64a53
wangtianwt <981003098@qq.cpm>
2020-05-11 16:18:01 +0800

    t

--- TimeSet_andorid/app/src/main/java/com/example/lt/timeset_andorid/util/Constant.java
+++ TimeSet_andorid/app/src/main/java/com/example/lt/timeset_andorid/util/Constant.java
@@ -1,10 +1,7 @@
 package com.example.lt.timeset_andorid.util;
 
 public class Constant {
-    public static final String IP ="http://192.168.0.7:8080/";
-    public static final   String URL="http://192.168.0.7:8080/";
-
-
-
-    public static final String CON_ADD_IMAGE_IP = "http://192.168.0.104:8080/";
+    public static final String IP ="http://192.168.1.9:8080/";
+    public static final   String URL="http://192.168.1.9:8080/";
+    public static final String CON_ADD_IMAGE_IP = "http://192.168.1.9:8080/";
 }
