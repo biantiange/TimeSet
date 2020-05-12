43b6e3133c505e00d82a855ff8d7014a604379b9
lupengjiao <lu15032742188@qq.com>
2020-05-12 11:00:46 +0800

    地图

--- TimeSet_andorid/app/src/main/java/com/example/lt/timeset_andorid/BigTwo/FootEarth/MapFragment.java
+++ TimeSet_andorid/app/src/main/java/com/example/lt/timeset_andorid/BigTwo/FootEarth/MapFragment.java
@@ -9,16 +9,27 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
 import com.example.lt.timeset_andorid.R;
 
 /**
  * 赵宁：足迹地球
  */
 public class MapFragment extends Fragment {
+    private MapView mapView = null;
+    private View newView = null;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View newView = inflater.inflate( R.layout.in_album_map, container, false );
+        //对百度地图SDK做初始化
+        // 在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        // 注意该方法要再setContentView方法之前实现
+        //参数： 环境上下文，最好是应用程序级别的上下文
+        SDKInitializer.initialize(this.getActivity().getApplicationContext());
+        newView = inflater.inflate( R.layout.in_album_map, container, false );
+        //获取地图控件引用
+        mapView = newView.findViewById(R.id.map);
         return newView;
     }
 
@@ -26,6 +37,27 @@ public class MapFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // 在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mapView.onDestroy();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        // 在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mapView.onResume();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        // 在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mapView.onPause();
     }
 
 }
