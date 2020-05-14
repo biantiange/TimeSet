bc480fb91806ebd5764a08f5b32493066b63ca1b
liuzhe0523 <1272991460@qq.com>
2020-05-14 11:35:11 +0800

    Update PhotoController.java

--- timeset/src/main/java/com/timeset/photo/controller/PhotoController.java
+++ timeset/src/main/java/com/timeset/photo/controller/PhotoController.java
@@ -56,7 +56,8 @@ public class PhotoController {
         String pa = UserController.class.getClassLoader().getResource("").getPath().split("timeset")[0];
         for (int i = 0; i < files.length; i++) {
             // 生成新的文件名
-            String fileName = System.currentTimeMillis() + files[i].getOriginalFilename();
+//            String fileName = System.currentTimeMillis() + files[i].getOriginalFilename();
+            String fileName = files[i].getOriginalFilename();
             // 保存路径
             String destFileName = ClassUtils.getDefaultClassLoader().getResource("").getPath() + "static/" + fileName;
             //String destFileName1 = request.getServletContext().getRealPath("") + "uploaded" + File.separator + fileName;
