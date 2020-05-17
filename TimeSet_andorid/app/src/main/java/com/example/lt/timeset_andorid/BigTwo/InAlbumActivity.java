100df5eff7d9e02df3497e0d669390f1bc8f1bfb
wangtianwt <981003098@qq.cpm>
2020-05-17 21:49:51 +0800

    t

--- TimeSet_andorid/app/src/main/java/com/example/lt/timeset_andorid/BigTwo/InAlbumActivity.java
+++ TimeSet_andorid/app/src/main/java/com/example/lt/timeset_andorid/BigTwo/InAlbumActivity.java
@@ -1,11 +1,11 @@
 package com.example.lt.timeset_andorid.BigTwo;
 
 
+import android.content.DialogInterface;
 import android.content.Intent;
-
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.graphics.Point;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -18,19 +18,22 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.lt.timeset_andorid.BigTwo.FootEarth.MapFragment;
+import com.example.lt.timeset_andorid.BigTwo.FootEarth.ShowPhotoInfoDialog;
 import com.example.lt.timeset_andorid.BigTwo.TimePhoto.CalendarFragment;
 import com.example.lt.timeset_andorid.BigTwo.TimePhoto.CalendarFragmentAdapter;
 import com.example.lt.timeset_andorid.BigTwo.TimePhoto.PhotoList;
 import com.example.lt.timeset_andorid.MainActivity;
+import com.example.lt.timeset_andorid.Entity.Photo;
 import com.example.lt.timeset_andorid.R;
 import com.example.lt.timeset_andorid.Search.SearchActivity;
 import com.example.lt.timeset_andorid.util.Constant;
@@ -52,10 +55,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeSet;
 
+import indi.liyi.viewer.ImageDrawee;
 import indi.liyi.viewer.ImageViewer;
 import indi.liyi.viewer.ViewData;
 import indi.liyi.viewer.ViewerStatus;
 import indi.liyi.viewer.listener.OnBrowseStatusListener;
+import indi.liyi.viewer.listener.OnItemChangedListener;
+import indi.liyi.viewer.listener.OnItemClickListener;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
@@ -67,6 +73,15 @@ import okhttp3.Response;
  * 赵宁：点击某个相册后，相册里面的展示
  */
 public class InAlbumActivity extends AppCompatActivity {
+    private RelativeLayout rlIverShow;
+    private TextView tvPhotoComment;
+
+    private RelativeLayout rlIverEdit;
+    private ImageView ivEditExit;
+    private TextView tvEditTime;
+    private ImageView ivEditDetail;
+    private ImageView ivEditDelete;
+
     private ImageButton return0;
     private TextView btn_search;
     private LinearLayout layout1;
@@ -83,15 +98,23 @@ public class InAlbumActivity extends AppCompatActivity {
     private SharedPreferences sharedPreferences;//获取用户信息
     private List<PhotoList> datasource=new ArrayList<>();
     int userId;
+    // 从EventBus获取的数据源
+    private int position;
+    private List<String> dataSource;
+    private List<Photo> photos;
     //抽屉
     private DrawerLayout mDrawer;
     private ListView TimeListView;
     private TimeAdapter tAdapter;
-    private List<Map<String,Object>> listTime=new ArrayList();
+    private List<Map<String, Object>> listTime = new ArrayList();
 
-    public int getId() { return id11; }
+    public int getId() {
+        return id11;
+    }
 
-    public void setId(int id1) { this.id11 = id1; }
+    public void setId(int id1) {
+        this.id11 = id1;
+    }
 
     private ImageButton addImg;
 
@@ -107,7 +130,6 @@ public class InAlbumActivity extends AppCompatActivity {
         //获取intent发送的相册id以及相册name
         EventBus.getDefault().register(this);
         id11 = getIntent().getIntExtra("id", -1);
-
         setId(id11);
 //        Log.e("id",getId()+"===================");
         initData();
@@ -127,40 +149,46 @@ public class InAlbumActivity extends AppCompatActivity {
         return super.onKeyDown(keyCode, event);
     }
 
+
     @Subscribe(threadMode = ThreadMode.MAIN, sticky = true)
     public void viewImage(Map<String, Object> showMap) {
         switch (showMap.get("type").toString()) {
             case "showImg":
-                int position = (int) showMap.get("position");
-                List<String> dataSource = (List<String>) showMap.get("datasource");
-                Log.e("Subscribe",dataSource.toString());
-                showBigImgs(position, dataSource);
+                position = (int) showMap.get("position");
+                dataSource = (List<String>) showMap.get("datasource");
+                photos = (List<Photo>) showMap.get("photoList");
+                String comment = photos.get(position).getPdescribe();
+                if (null != comment)
+                    tvPhotoComment.setText(comment);
+                else {
+                    tvPhotoComment.setText("");
+                }
+                rlIverShow.setVisibility(View.VISIBLE);
+                iver.setBackgroundColor(Color.parseColor("#000000"));
+                showBigImgs();
                 break;
         }
     }
 
     // 展示图片
-    private void showBigImgs(int position, List<String> showImgSource) {
+    private void showBigImgs() {
         iver.setVisibility(View.VISIBLE);
         llOut.setVisibility(View.GONE);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             iver.setElevation(3.0f);
         }
         List<ViewData> vdList = new ArrayList<>();
-        Point mScreenSize = ViewDataUtils.getScreenSize(getApplicationContext());
-        for (int i = 0, len = showImgSource.size(); i < len; i++) {
+        Point mScreenSize = ViewDataUtils.getScreenSize(this);
+        for (int i = 0, len = dataSource.size(); i < len; i++) {
             ViewData viewData = new ViewData();
-            viewData.setImageSrc(showImgSource.get(i));
+            viewData.setImageSrc(dataSource.get(i));
             viewData.setTargetX(0);
             viewData.setTargetY(0);
             viewData.setTargetWidth(mScreenSize.x);
-            viewData.setTargetHeight(ViewDataUtils.dp2px(getApplicationContext(), 200));
+            viewData.setTargetHeight(ViewDataUtils.dp2px(this, 200));
             vdList.add(viewData);
         }
-        for (ViewData data : vdList) {
-//            Log.e("vdList",""+data.getImageSrc().toString());
-        }
-        iver.overlayStatusBar(false) // ImageViewer 是否会占据 StatusBar 的空间
+        iver.overlayStatusBar(true) // ImageViewer 是否会占据 StatusBar 的空间
                 .viewData(vdList) // 数据源
                 .imageLoader(new PhotoLoader()) // 设置图片加载方式
                 .playEnterAnim(true) // 是否开启进场动画，默认为true
@@ -171,16 +199,151 @@ public class InAlbumActivity extends AppCompatActivity {
 //                            .loadProgressUI(progressUI) // 自定义图片加载进度样式，内置默认样式
                 .watch(position);
 
+        // 监听单击事件
+        iver.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public boolean onItemClick(final int i, ImageView imageView) { // 将展示修改界面
+                if (rlIverShow.getVisibility() == View.VISIBLE) {
+
+                    rlIverShow.setVisibility(View.GONE);    // 展示界面消失
+                    rlIverEdit.setVisibility(View.VISIBLE);
+                    iver.setBackgroundColor(Color.parseColor("#ffffff"));  // 背景色变白
+                    // 展示修改界面显示
+                    ivEditExit.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            iver.cancel();
+                        }
+                    });
+                    String time = photos.get(i).getPtime();
+                    if (null != time) {
+                        tvEditTime.setText(time.substring(0, 4) + "年" + time.substring(4, 6) + "月" + time.substring(6, 8) + "日");
+                    } else {
+                        tvEditTime.setText("");
+                    }
+                    ivEditDetail.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            ShowPhotoInfoDialog dialog = ShowPhotoInfoDialog.getDialog();
+                            dialog.setContext(InAlbumActivity.this);
+                            dialog.setPhoto(photos.get(i));
+                            dialog.showBottomDialog();
+                        }
+                    });
+                    ivEditDelete.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            showDeleteDialog(i, photos.get(i));
+                        }
+                    });
+                } else if (rlIverEdit.getVisibility() == View.VISIBLE) {  // 将展示展示界面
+                    rlIverEdit.setVisibility(View.GONE);
+                    rlIverShow.setVisibility(View.VISIBLE); // 展示界面显示
+                    iver.setBackgroundColor(Color.parseColor("#000000"));// 背景色变黑
+                    // 修改comment
+                    String comment = photos.get(i).getPdescribe();
+                    if (null != comment)
+                        tvPhotoComment.setText(comment);
+                    else {
+                        tvPhotoComment.setText("");
+                    }
+                }
+                return true;    // 返回值为true，不会退出展示
+            }
+        });
+        // 切换图片
+        iver.setOnItemChangedListener(new OnItemChangedListener() {
+            @Override
+            public void onItemChanged(int i, ImageDrawee drawee) {
+                ivEditDetail.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ShowPhotoInfoDialog dialog = ShowPhotoInfoDialog.getDialog();
+                        dialog.setContext(InAlbumActivity.this);
+                        dialog.setPhoto(photos.get(i));
+                        dialog.showBottomDialog();
+                    }
+                });
+                if (rlIverShow.getVisibility() == View.VISIBLE) {
+                    String comment = photos.get(i).getPdescribe();
+                    if (null != comment)
+                        tvPhotoComment.setText(comment);
+                    else {
+                        tvPhotoComment.setText("");
+                    }
+                }
+            }
+        });
         iver.setOnBrowseStatusListener(new OnBrowseStatusListener() {
             @Override
             public void onBrowseStatus(int status) {
                 if (status == ViewerStatus.STATUS_BEGIN_OPEN) {
                     // 正在开启启动预览图片
                     llOut.setVisibility(View.GONE);
+                    iver.setVisibility(View.VISIBLE);
+                    rlIverShow.setVisibility(View.VISIBLE);
+                    rlIverEdit.setVisibility(View.GONE);    // 进来时展示展示界面
                 } else if (status == ViewerStatus.STATUS_SILENCE) {
                     // 此时未开启预览图片
                     llOut.setVisibility(View.VISIBLE);
+                    iver.setVisibility(View.GONE);
+                    rlIverShow.setVisibility(View.GONE);
+                    rlIverEdit.setVisibility(View.GONE);
                 }
+            }
+        });
+    }
+
+    private void showDeleteDialog(int i, Photo photo) {
+        // 显示一个Dialog
+        AlertDialog.Builder adBuilder = new AlertDialog.Builder(this);
+        adBuilder.setTitle("确定删除此图片");
+
+        adBuilder.setPositiveButton("确认删除", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // 1. 删除数据源
+                Map<String, String> deleteMap = new HashMap<>();
+                deleteMap.put("type", "delete");
+                deleteMap.put("position", i + "");
+                deleteMap.put("time", photo.getPtime());
+                EventBus.getDefault().post(deleteMap);
+                // 2. 关闭iver
+                iver.cancel();
+                // 3. 修改数据库
+                changeDelete(photo.getId());
+            }
+        });
+        adBuilder.setNegativeButton("我手滑了", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // 选中“取消”按钮，取消界面
+            }
+        });
+        adBuilder.create().show();
+    }
+
+    private static final String IP = Constant.IP + "/photo/delete";
+
+    private void changeDelete(int id) {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        FormBody.Builder builder = new FormBody.Builder().add("photoId", id + "");
+        FormBody body = builder.build();
+        Request request = new Request.Builder().post(body).url(IP).build();
+        final Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+//                e.printStackTrace();
+//                Toast.makeText(context,"delete失败",Toast.LENGTH_SHORT).show();
+                Log.e("deleteee", "失败");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+//                String strings = response.body().string();
+//                Toast.makeText(context,"1",Toast.LENGTH_SHORT).show();
+                Log.e("deletee", "成果");
             }
         });
     }
@@ -198,7 +361,6 @@ public class InAlbumActivity extends AppCompatActivity {
         map.get(tabStrId[1]).setFragment(new MapFragment());
     }
 
-
     //时间轴
     private void findView() {
         layout1 = findViewById(R.id.tab_spec_1);
@@ -212,7 +374,15 @@ public class InAlbumActivity extends AppCompatActivity {
         albumName.setText(getIntent().getStringExtra("albumName"));
         //抽屉
         mDrawer = findViewById(R.id.drawer_layout);
-        TimeListView =findViewById(R.id.list_time);
+        TimeListView = findViewById(R.id.list_time);
+        // 展示
+        rlIverShow = findViewById(R.id.rl_in_album_iver_show_out);
+        tvPhotoComment = findViewById(R.id.tv_in_album_mark_item_comment);
+        rlIverEdit = findViewById(R.id.rl_in_album_iver_show_edit_out);
+        ivEditExit = findViewById(R.id.iv_in_album_iver_show_edit_exit);
+        tvEditTime = findViewById(R.id.tv_in_album_iver_show_edit_time);
+        ivEditDetail = findViewById(R.id.iv_in_album_iver_show_edit_detail);
+        ivEditDelete = findViewById(R.id.iv_in_album_iver_show_edit_delete);
 
     }
 
@@ -258,6 +428,11 @@ public class InAlbumActivity extends AppCompatActivity {
           //  Log.e("tttt",);
             map.put("time",str);
             map.put("statu",0);
+    private void findTime() {
+        for (int i = 0; i < 9; i++) {
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("time", "11");
+            map.put("statu", 0);
             listTime.add(map);
         }
         tAdapter = new TimeAdapter(this, listTime);
@@ -265,6 +440,7 @@ public class InAlbumActivity extends AppCompatActivity {
         TimeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
                 EventBus.getDefault().post(listTime.get(position).get("time"));
                 listTime.get(position).put("statu",1) ;
                 tAdapter.notifyDataSetChanged();
--- TimeSet_andorid/app/src/main/java/com/example/lt/timeset_andorid/BigTwo/TimePhoto/CalendarFragment.java
+++ TimeSet_andorid/app/src/main/java/com/example/lt/timeset_andorid/BigTwo/TimePhoto/CalendarFragment.java
@@ -1,8 +1,6 @@
 package com.example.lt.timeset_andorid.BigTwo.TimePhoto;
 
-import android.app.Notification;
 import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -17,31 +15,30 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.example.lt.timeset_andorid.Login.LoginActivity;
+import com.example.lt.timeset_andorid.Entity.Photo;
 import com.example.lt.timeset_andorid.R;
 import com.example.lt.timeset_andorid.util.Constant;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
-import com.mob.tools.utils.Strings;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import indi.liyi.viewer.ImageViewer;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.RequestBody;
 import okhttp3.Response;
 
 import static android.content.Context.MODE_PRIVATE;
@@ -52,6 +49,7 @@ import static android.content.Context.MODE_PRIVATE;
 public class CalendarFragment extends Fragment {
     private SharedPreferences sharedPreferences;//获取用户信息
     private List<PhotoList> datasource = new ArrayList<>();
+    private CalendarFragmentAdapter calendarFragmentAdapter;
     private ListView listView;
     int albumId;
     int userId;
@@ -59,9 +57,29 @@ public class CalendarFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View newView = inflater.inflate(R.layout.in_album_calendar, container, false);
+
+        View newView = inflater.inflate( R.layout.in_album_calendar, container, false );
+
         EventBus.getDefault().register(this);
         return newView;
+    }
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void deleteItem(Map<String,String> deleteMap){
+        if (deleteMap.get("type").equals("delete")){
+            int position = Integer.parseInt(deleteMap.get("position"));
+            String time = deleteMap.get("time");
+            for (int i=0;i<datasource.size();i++){
+                PhotoList pl = datasource.get(i);
+                if (pl.getPtime().equals(time)){
+                    pl.getPhotoList().remove(position);
+                    if (pl.getPhotoList().size() == 0){
+                        datasource.remove(pl);
+                    }
+                    break;
+                }
+            }
+            calendarFragmentAdapter.notifyDataSetChanged();
+        }
     }
 
     @Override
@@ -69,9 +87,10 @@ public class CalendarFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         listView = getActivity().findViewById(R.id.in_album_list_items);
         //获取相册id值//根据用户id或者用户手机号以及相册id查询相册数据
-        albumId = getActivity().getIntent().getIntExtra("albumId", -1);
-        sharedPreferences = getContext().getSharedPreferences("user", MODE_PRIVATE);
-        userId = sharedPreferences.getInt("id", -1);
+
+        albumId=getActivity().getIntent().getIntExtra("albumId",-1);
+        sharedPreferences=getContext().getSharedPreferences("user",MODE_PRIVATE);
+        userId=sharedPreferences.getInt("id",-1);
         OkHttpClient okHttpClient = new OkHttpClient();
         if (albumId != -1 && userId != -1) {
             FormBody.Builder builder = new FormBody.Builder().add("albumId", String.valueOf(albumId)).add("userId", String.valueOf(userId));
@@ -98,24 +117,23 @@ public class CalendarFragment extends Fragment {
             });
         }
     }
+   private Handler handler=new Handler(){
+       @Override
+       public void handleMessage(Message msg) {
+           if (msg.what==1){
+               Log.e("======","执行失败");
+           }
+           if(msg.what==2){
 
-    private Handler handler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            if (msg.what == 1) {
-                Log.e("======", "执行失败");
-            }
-            if (msg.what == 2) {
+               Gson gson=new GsonBuilder().serializeNulls().create();
+               List<PhotoList> list= gson.fromJson(msg.obj.toString(),new TypeToken<List<PhotoList>>() {}.getType());
+               datasource=list;
+               Log.e("s=====================",list.size()+""+list.toString());
+               calendarFragmentAdapter=new CalendarFragmentAdapter(getContext(),datasource, R.layout.in_album_list_item);
+               listView.setAdapter(calendarFragmentAdapter);
+           }
 
-                Gson gson = new GsonBuilder().serializeNulls().create();
-                List<PhotoList> list = gson.fromJson(msg.obj.toString(), new TypeToken<List<PhotoList>>() {
-                }.getType());
-                datasource = list;
-                Log.e("s=====================", list.size() + "" + list.toString());
-                CalendarFragmentAdapter calendarFragmentAdapter = new CalendarFragmentAdapter(getContext(), datasource, R.layout.in_album_list_item);
-                listView.setAdapter(calendarFragmentAdapter);
-            }
-
+   
         }
     };
 
@@ -138,9 +156,68 @@ public class CalendarFragment extends Fragment {
 
     }
 
+//    private void getData() {
+//        // 1. 获取照片数据( 模拟通过intent获取phonoList数据源)
+//        datasource = new ArrayList<>();
+//        Photo photo = new Photo();
+//        photo.setPlace("北京");
+//        photo.setPtime("20200202");
+//        photo.setPdescribe("asdsadf");
+//        photo.setPath("https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1906469856,4113625838&fm=26&gp=0.jpg");
+//        Photo photo1 = new Photo();
+//        Photo photo4 = new Photo();
+//        photo4.setPlace("北京");
+//        photo4.setPtime("20200202");
+//        photo4.setPdescribe("asdsggggadf");
+//        photo4.setPath("https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1906469856,4113625838&fm=26&gp=0.jpg");
+//        Photo photo5 = new Photo();
+//        photo5.setPlace("北京");
+//        photo5.setPtime("20200202");
+//        photo5.setPdescribe("asdsggggadf123");
+//        photo5.setPath("https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1906469856,4113625838&fm=26&gp=0.jpg");
+//
+//        Photo photo6 = new Photo();
+//        photo6.setPlace("北京");
+//        photo6.setPtime("20200202");
+//        photo6.setPath("https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1906469856,4113625838&fm=26&gp=0.jpg");
+//
+//        photo1.setPlace("上海");
+//        photo1.setPtime("20200202");
+//        photo1.setPath("https://dss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2534506313,1688529724&fm=26&gp=0.jpg");
+//        Photo photo2 = new Photo();
+//        photo2.setPlace("广西");
+//        photo2.setPtime("20200203");
+//        photo2.setPath("https://dss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3252521864,872614242&fm=26&gp=0.jpg");
+//        Photo photo3 = new Photo();
+//        photo3.setPlace("成都");
+//        photo3.setPtime("20200204");
+//        photo3.setPath("https://dss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3252521864,872614242&fm=26&gp=0.jpg");
+//        List<Photo> photoList = new ArrayList<>();
+//        photoList.add(photo);
+//        photoList.add(photo1);
+//        photoList.add(photo4);
+//        photoList.add(photo5);
+//        photoList.add(photo6);
+//        List<Photo> photos1 = new ArrayList<>();
+//        photos1.add(photo2);
+//        List<Photo> photos2 = new ArrayList<>();
+//        photos2.add(photo3);
+//        PhotoList photoList1 = new PhotoList();
+//        photoList1.setPhotoList(photoList);
+//        photoList1.setPtime("20200202");
+//        PhotoList photoList2 = new PhotoList();
+//        photoList2.setPhotoList(photos1);
+//        photoList2.setPtime("20200203");
+//        PhotoList photoList3 = new PhotoList();
+//        photoList3.setPhotoList(photos2);
+//        photoList3.setPtime("20200204");
+//        datasource.add(photoList1);
+//        datasource.add(photoList2);
+//        datasource.add(photoList3);
+//    }
     @Override
     public void onDestroy() {
-        EventBus.getDefault().unregister(this);
         super.onDestroy();
+        EventBus.getDefault().unregister(this);
     }
 }
