557253f486256907113e98b8c040054a07aa4db8
biantiange <1329139497@qq.com>
2020-05-19 21:11:47 +0800

    Fetch_8

--- TimeSet_andorid/app/src/main/java/com/example/lt/timeset_andorid/util/Constant.java
+++ TimeSet_andorid/app/src/main/java/com/example/lt/timeset_andorid/util/Constant.java
@@ -1,11 +1,9 @@
-﻿package com.example.lt.timeset_andorid.util;
+package com.example.lt.timeset_andorid.util;
 
 public class Constant {
 
-
     public static final String IP ="http://192.168.0.7:8080/";
     public static final   String URL="http://192.168.0.7:8080/";
-
 
     public static final String AK="jN6xIoPtbQK52In8Odt288FimIM4PgiX";
     public static final String MCODE="83:E7:2C:1C:95:EB:96:64:B7:1A:54:BA:5C:E0:77:88:0D:37:80:E4;com.example.lt.timeset_andorid";
